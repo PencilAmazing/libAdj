05510f82028bb5a8363db9ff21b1a5b44ec671c8
Pencil Amazing <saifshw@live.com>
2022-03-02 21:53:07 -0400

    Fix null pointer issues

--- LibAdj.h
+++ LibAdj.h
@@ -14,18 +14,13 @@ namespace libAdj {
         HalfEdgeShort* next = nullptr;
     } HalfEdgeShort;
 
-    typedef struct HalfEdgeLarge {
-        unsigned int index;
-        HalfEdgeLarge* twin = nullptr;
-        HalfEdgeLarge* next = nullptr;
-    } HalfEdgeLarge;
-
-    std::map<unsigned short, HalfEdgeShort*> GenerateHalfEdgeGraphShort(const std::vector<unsigned short> indices, unsigned int triCount = 0)
+    std::map<unsigned int, HalfEdgeShort> GenerateHalfEdgeGraphShort(const std::vector<unsigned short> indices, std::vector<unsigned int>& keyList, unsigned int triCount = 0)
     {
+
         if (triCount == 0) triCount = indices.size() / 3;
-        std::map<unsigned short, HalfEdgeShort*> edgeMap; // Three edges per triangle
-        std::vector<HalfEdgeShort> edgeList;
-        edgeList.reserve(triCount * 3);
+        std::map<unsigned int, HalfEdgeShort> edgeMap; // Three edges per triangle
+        keyList.clear();
+        keyList.resize(triCount * 3);
 
         // Fill in the edge indices and their keys
         for (size_t face = 0; face < triCount; face++) {
@@ -33,9 +28,13 @@ namespace libAdj {
             unsigned short v2 = indices[face * 3 + 1];
             unsigned short v3 = indices[face * 3 + 2];
 
-            HalfEdgeShort* edge1 = &edgeList[face * 3 + 0];
-            HalfEdgeShort* edge2 = &edgeList[face * 3 + 1];
-            HalfEdgeShort* edge3 = &edgeList[face * 3 + 2];
+            unsigned int key1 = (unsigned int)v3 | (unsigned int)v1 << 16u;
+            unsigned int key2 = (unsigned int)v1 | (unsigned int)v2 << 16u;
+            unsigned int key3 = (unsigned int)v2 | (unsigned int)v3 << 16u;
+
+            HalfEdgeShort* edge1 = &edgeMap[key1];
+            HalfEdgeShort* edge2 = &edgeMap[key2];
+            HalfEdgeShort* edge3 = &edgeMap[key3];
 
             edge1->index = v1;
             edge1->next = edge2;
@@ -46,57 +45,44 @@ namespace libAdj {
             edge3->index = v3;
             edge3->next = edge1;
 
-            edgeMap[(unsigned int)v1 | (unsigned int)v2 << 16u] = edge1;
-            edgeMap[(unsigned int)v2 | (unsigned int)v3 << 16u] = edge2;
-            edgeMap[(unsigned int)v3 | (unsigned int)v1 << 16u] = edge3;
+            keyList[face * 3 + 0] = key1;
+            keyList[face * 3 + 1] = key2;
+            keyList[face * 3 + 2] = key3;
         }
 
-        // Read the edges
-        for (std::pair<unsigned int, HalfEdgeShort*> edge : edgeMap) {
-            // If twin was already filled in then skip
-            if (edge.second->twin)
-                continue;
-
-            unsigned int key = edge.first;
+        for (unsigned int key : keyList) {
             unsigned int twinKey = (key << 16u) | (key >> 16u);
-
-            // If twin does not exist, then mesh has holes and is invalid
             if (edgeMap.find(twinKey) == edgeMap.end()) {
-                // Return empty set
+                keyList.clear();
                 return {};
             } else {
-                // Fill in twin references
-                HalfEdgeShort* twinEdge = edgeMap[twinKey];
-                twinEdge->twin = edge.second;
-                edge.second->twin = twinEdge;
+                edgeMap[key].twin = &edgeMap[twinKey];
+                edgeMap[twinKey].twin = &edgeMap[key];
             }
         }
+
         return edgeMap;
     }
 
-    std::vector<unsigned short> CalculateAdjacencyShort(const std::map<unsigned short, HalfEdgeShort*> edgeList)
+    std::vector<unsigned short> CalculateAdjacencyShort(std::map<unsigned int, HalfEdgeShort>& edgeList, std::vector<unsigned int>& keyList)
     {
         unsigned edgeCount = edgeList.size();
-        std::vector<unsigned short> indexBuffer(edgeCount * 2); // Two indices per edge
-
+        std::vector<unsigned short> indexBuffer; // Two indices per edge
+        indexBuffer.resize(edgeCount * 2);
         unsigned triCount = edgeCount / 3;
 
-        // Assuming the edges here are ordered, every consecutive three is a triangle
-        // Push the triangle vert then the adjacent vert
-        for (auto edge = edgeList.begin(); edge != edgeList.end(); edge++) {
-            auto& edge1 = edge->second;
-            auto& edge2 = (edge++)->second;
-            auto& edge3 = (edge++)->second;
+        for (unsigned int face = 0; face < triCount; face++) {
+            auto& edge1 = edgeList[keyList[face * 3 + 0]];
+            auto& edge2 = edgeList[keyList[face * 3 + 1]];
+            auto& edge3 = edgeList[keyList[face * 3 + 2]];
 
             // We start from 3 to preserve index order
-            indexBuffer.push_back(edge3->index);
-            indexBuffer.push_back(edge3->twin->next->index);
-
-            indexBuffer.push_back(edge1->index);
-            indexBuffer.push_back(edge1->twin->next->index);
-
-            indexBuffer.push_back(edge2->index);
-            indexBuffer.push_back(edge2->twin->next->index);
+            indexBuffer[face * 6 + 0] = edge1.index;
+            indexBuffer[face * 6 + 1] = edge2.twin->next->index;
+            indexBuffer[face * 6 + 2] = edge2.index;
+            indexBuffer[face * 6 + 3] = edge3.twin->next->index;
+            indexBuffer[face * 6 + 4] = edge3.index;
+            indexBuffer[face * 6 + 5] = edge1.twin->next->index;
         }
 
         return indexBuffer;
@@ -104,8 +90,10 @@ namespace libAdj {
 
     std::vector<unsigned short> CalculateAdjacencyShort(const std::vector<unsigned short> indexBuffer, unsigned triCount = 0)
     {
-        auto HalfEdgeGraph = GenerateHalfEdgeGraphShort(indexBuffer, triCount);
-        return CalculateAdjacencyShort(HalfEdgeGraph);
+        std::vector<unsigned int> keyList;
+
+        auto HalfEdgeGraph = GenerateHalfEdgeGraphShort(indexBuffer, keyList, triCount);
+        return CalculateAdjacencyShort(HalfEdgeGraph, keyList);
     }
 
 }
